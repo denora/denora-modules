8fedf5cba620918410727f64e1f78ccdb684e6f5
Hal9000 <hal9000@denorastats.org>
2012-06-27 19:55:19 +0200

    initial import

---
+++ ss_restricthelp/ss_restricthelp.c
@@ -0,0 +1,34 @@
+/*
+ * ss_restricthelp module for Denora 1.4+
+ * (c) Jobe (jobe@mdbnet.co.uk)
+ */
+
+#include "denora.h"
+#define AUTHOR "Jobe"
+#define VERSION "1.0.1"
+#define MYNAME "ss_restricthelp"
+
+int DenoraInit(int argc, char **argv);
+void DenoraFini(void);
+int do_help(User * u, int argc, char **argv);
+
+int DenoraInit(int argc, char **argv)
+{
+  Command *c;
+  c = createCommand("HELP", do_help, is_oper, -1, -1, -1, -1);
+  moduleAddCommand(STATSERV, c, MOD_HEAD);
+
+  moduleAddAuthor(AUTHOR);
+  moduleAddVersion(VERSION);
+  return MOD_CONT;
+}
+
+void DenoraFini(void)
+{
+  alog(LOG_NORMAL, "Unloading ss_restricthelp%s", MODULE_EXT);
+}
+
+int do_help(User * u, int argc, char **argv)
+{
+  return MOD_CONT;
+}
